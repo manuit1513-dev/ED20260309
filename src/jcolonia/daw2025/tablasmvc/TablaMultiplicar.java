c95ca31a65ccb097177d90ea0e9539381d0e28f3
Manuel David García Prado <manu.it1513@gmail.com>
2026-03-10 12:34:08 +0100

    Javadoc y metodos completados

--- src/jcolonia/daw2025/tablasmvc/TablaMultiplicar.java
+++ src/jcolonia/daw2025/tablasmvc/TablaMultiplicar.java
@@ -1,28 +1,73 @@
 package jcolonia.daw2025.tablasmvc;
 
+import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Clase encargada de generar la tabla de multiplicar.
+ */
 public class TablaMultiplicar {
+	/**
+	 * Número del que va a ser la tabla de multiplicar.
+	 */
 	private int numero;
+	/**
+	 * Lista donde se guardan los resultados de cada operación.
+	 */
 	private List<String> listaTextos;
-	
+	/**
+	 * Construye la tabla de multiplicar
+	 * @param numero el número al que pertenece la tabal de multiplicar
+	 */
 	public TablaMultiplicar(int numero) {
-		
+		this.numero = numero;
+		this.listaTextos = new ArrayList<>();
 	}
 	
+	/**
+	 * Genera la tabla de multiplicar del número introducido (del 0 al 10).
+	 * <p>
+	 * Calcula cada operación y las formatea como cadenas de texto ejemplo ->("5 × 2 = 10") 
+	 * y las almacena secuencialmente en la lista de textos creada.
+	 * </p>
+	 * 
+	 * @see #listaTextos
+	 */
 	public void generarTabla() {
-		
+		for(int i = 0;i <= 10; i++) {
+			int resultado;
+			
+			resultado = numero * i;
+			
+			String linea = String.format("%d × %d = %d", numero, i, resultado);
+			listaTextos.add(linea);
+		}
 	}
 	
+	/**
+	 * 
+	 * @return la lista sin formatear para exportar a archivo txt.
+	 */
 	public List<String> toListaExportacion(){
-		
+		return new ArrayList<>(listaTextos);
 	}
 	
+	/**
+	 * Da formato a la lista.
+	 * @return La lista para que sea devuelta en pantalla
+	 */
 	public List<String> toListaPantalla(){
+		List<String> listaPantalla = new ArrayList<>();
+		listaPantalla.add("=== Tabla del " + numero + "===");
+		listaPantalla.addAll(listaTextos);
+		listaPantalla.add("===================");
 		
+		return listaPantalla;
 	}
-	
+	/**
+	 * 
+	 */
+	@Override
 	public String toString() {
-		
+		return String.join("\n", toListaPantalla());
 	}
 }
